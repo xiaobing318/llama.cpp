f5eae8490403e1f10412767ebd4df46803dc66cf
xiaobing318 <71554036+xiaobing318@users.noreply.github.com>
2025-08-03 23:23:57 +0800

    feat:添加注释以便理解代码逻辑

--- examples/server/webui/src/App.tsx
+++ examples/server/webui/src/App.tsx
@@ -1,4 +1,6 @@
+//  使用 import 语句从 react-router 库中导入 HashRouter、Outlet、Route 和 Routes 组件，这些组件用于设置应用的路由。
 import { HashRouter, Outlet, Route, Routes } from 'react-router';
+// 使用 Module 特性从当前目录下的组件文件中导入 AppLayout、Header、Sidebar、ChatScreen 和 SettingDialog 组件，这些组件构成了应用的主要界面和功能。
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import { AppContextProvider, useAppContext } from './utils/app.context';
--- examples/server/webui/src/main.tsx
+++ examples/server/webui/src/main.tsx
@@ -1,8 +1,17 @@
+// 使用 Module 特性从 React 库中导入 StrictMode 元素以供后续使用。
 import { StrictMode } from 'react';
+// 使用 Module 特性从 React DOM 库中导入 createRoot 函数，用于创建 React 应用的根节点。
 import { createRoot } from 'react-dom/client';
+// 使用 Module 特性从当前目录下的 index.scss 文件中导入样式表，以便在应用中使用这些样式。
 import './index.scss';
+// 使用 Module 特性从当前目录下的 App.tsx 文件中导入 App 组件，这个组件是应用的主要部分。
 import App from './App.tsx';
 
+/*
+Note:杨小兵-2025-08-03
+    使用 createRoot 函数创建一个 React 应用的根节点，并将其挂载到 HTML 文档中的 id 为 'root' 的元素上。
+通过 StrictMode 包裹 App 组件，以便在开发模式下启用额外的检查和警告，帮助开发者发现潜在的问题。
+*/
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
