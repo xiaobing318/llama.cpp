8b67a797c3eff36accac13b80fbd1990a8d598d0
xiaobing318 <71554036+xiaobing318@users.noreply.github.com>
2025-08-19 16:46:22 +0800

    feat:上传 webui 前端代码

---
+++ tools/QCopilot/webui/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    /*modify：将主题仅保留 light/dark两种主题*/
+    themes: ['light', 'dark'],
+  }
+}
